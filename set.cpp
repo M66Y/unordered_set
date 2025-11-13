68d85659d62dec61db9c724f05cdfd478de0e863
M66Y <2956910087@qq.com>
2025-11-13 14:38:35 +0800

    初次提交

---
+++ set.cpp
@@ -0,0 +1,33 @@
+// 引入库
+#include <iostream>
+#include <unordered_set>
+// #include <set>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class setSolution
+{
+private:
+    // 根据public部分的需求，设计相应的私有成员变量和私有成员函数
+    unordered_set<int> mySet;
+
+public:
+    // 构造函数
+
+    // 成员函数
+
+    // 求两个集合的交、并和差集
+
+    // 求集合的幂集
+
+    // 求集合的n元子集
+};
+
+int main()
+{
+
+    return 0;
+}
